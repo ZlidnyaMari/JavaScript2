c7a195c173c40e396fc5f6d1f2afd04f68ec891a
Zlidnya <Kid08@mail.ru>
2021-06-11 17:49:37 +0300

    Задание7

--- new_app/src/App.vue
+++ new_app/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
       <Header @toggle-cart = 'toggleCart' @filter-goods = 'filterGoods' />
     <main>
-        <GoodsList :goods = "filteredGoods" />
-        <Cart :isVisibleCart = 'isVisibleCart'/>
+        <GoodsList @add-to-cart = 'addToCart' :goods = "filteredGoods" />
+        <Cart :cartGoods = 'cartGoods' :isVisibleCart = 'isVisibleCart'/>
     </main>     
   </div>
 </template>
@@ -12,7 +12,8 @@
 import GoodsList from './components/GoodsList';
 import Header from './components/Header';
 import Cart from './components/Cart';
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+
+const API_URL = 'http://localhost:3000'
 
 export default {
 components: {
@@ -22,21 +23,47 @@ components: {
 },
 data: () => ({
   goods: [],
+  cartGoods: [],
   filteredGoods: [],
   isVisibleCart: false,
 }),
 mounted() {
-this.makeGETRequest (`${API_URL}/catalogData.json`)
+this.getGoods();
+this.getCart();
 },
 methods: {
+    addToCart(item) {
+        this.makePOSTRequest(`${API_URL}/addToCart`, item)
+        .then(() => this.getCart())
+},
     makeGETRequest(url) {
-        fetch(url)
+        return fetch(url)
             .then((data) => data.json())
-            .then((data) => {
-            this.goods = data;
-            this.filteredGoods = data;
-      })
   },
+  	makePOSTRequest(url, data) {
+        return fetch(url, {
+        method: 'POST',
+        headers: {
+        // добавили хэдер
+        'Content-Type': 'application/json'
+},
+    body: JSON.stringify(data),
+})
+    .then((data) => data.json())
+},
+    getGoods() {
+        this.makeGETRequest(`${API_URL}/catalogData`)
+            .then((data) => {
+        this.goods = data;
+        this.filteredGoods = data;
+    })
+},
+    getCart() {
+        this.makeGETRequest(`${API_URL}/cartData`)
+        .then((data) => {
+        this.cartGoods = data
+    })
+},
     filterGoods(value) {
         const regexp = new RegExp(value, 'i');
         this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
@@ -45,11 +72,6 @@ methods: {
         this.isVisibleCart = !this.isVisibleCart;
     },
   },
-   watch: {
-        searhLine(){
-            this.filterGoods();
-        }
-   }
 }
   
 </script>
